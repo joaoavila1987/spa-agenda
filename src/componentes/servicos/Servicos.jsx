223a6bc80ecea19e62f42ff47fcab426b031fcdd
joaoavila <joaoavila.py@gmail.com>
2023-01-23 21:31:14 -0300

    pagina serviços inicializada

--- src/componentes/servicos/Servicos.jsx
+++ src/componentes/servicos/Servicos.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import NavBar from '../navbar/NavBar';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+//import Box from '@mui/material/Box';
 
 export default function Servicos() {
   return (
     <>
     <NavBar />
-    
+    <br/>
+    <Grid container justifyContent={'center'}>
+    <Grid item xs={12}>
+     <Typography textAlign="center" variant='h6' sx={{ color : 'black'}} >Serviços</Typography>
+    </Grid>
+    </Grid>
+    <br />
+    <Grid container justifyContent={'center'} spacing={3}>
+    <Grid item  xs={12} md={3}>
+    <TextField fullWidth size='large' label="Descrição do serviço"  />
+    </Grid>
+      <Grid item xs={12} md={3}>
+     <TextField fullWidth size='large' label="Valor"  />
+    </Grid>
+      <Grid item xs={12} md={3}>
+      <Button
+        variant="contained"
+        sx={{ my: 2, color: 'white', bgcolor: 'black',  '&:hover': {
+            backgroundColor: '#ccc',
+            borderColor: '#ccc',
+            boxShadow: 'none',  
+            color: '#000'
+          },}}
+          startIcon={<AddIcon />}>
+        Adicionar
+    </Button>
+    </Grid>
+    </Grid>
+
+
+
+      
     </>
   )
 }
