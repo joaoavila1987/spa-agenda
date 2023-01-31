d376fd6c79d2a015f489c97347582265506af1aa
joaoavila <joaoavila.py@gmail.com>
2023-01-30 21:58:08 -0300

    mais algumas modificações na tela de serviços

--- src/componentes/servicos/TabelaServico.jsx
+++ src/componentes/servicos/TabelaServico.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+//import IconButton from '@mui/material/IconButton';
 
 
 export default function TabelaServico() {
@@ -9,22 +10,31 @@ export default function TabelaServico() {
         { 
           field: 'id', 
           headerName: 'ID', 
-          width: 90 
+          width: 90,
+          editable: false
         },
 
         {
           field:'descricao',
           headerName: 'Descrição',
           width: 300,
-          editable: false,
+          editable: false
+        },
+        {
+          field:'deletar',
+          headerName: 'Excluir',
+          width: 50,
+          editable: false
         }
       ];
       
       const listaServicos = [
+
         { id: 1, descricao: 'Corte de cabelo' },
         { id: 2, descricao: 'Barba complera' },
         { id: 3, descricao: 'Hidratação' },
         { id: 4, descricao: 'Tratamento de Tintura' }
+        
         ]
 
   return (
